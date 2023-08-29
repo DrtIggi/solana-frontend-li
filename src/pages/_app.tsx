5b7b006661c9eb7d049a98353807de01054c0e2e
Ignat <i3378047@gmail.com>
2023-08-29 15:03:37 +0200

    update app, include components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { ContextProvider } from '../contexts/ContextProvider';
 import { AppBar } from '../components/AppBar';
+import DiceRoll from '../components/DiceRoll'
+import BetInput from 'components/BetInput';
+import WinNotification from 'components/WinNotification'
+import Recent from 'components/Recent'
 // import { ContentContainer } from '../components/ContentContainer';
 // import { Footer } from '../components/Footer';
 // import Notifications from '../components/Notification'
@@ -10,6 +14,15 @@ require('@solana/wallet-adapter-react-ui/styles.css');
 require('../styles/globals.css');
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
+  const [diceValue, setDiceValue] = useState(1);
+  const [inputValue, setInputValue] = useState(0);
+
+  const handleDiceValueChange = (newValue) => {
+    setDiceValue(newValue);
+  };
+  const handleInputValueChange = (newValue) => {
+    setInputValue(newValue);
+  }
     return (
         <>
           <Head>
@@ -24,10 +37,15 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
                 {/* <Component {...pageProps} /> */}
                 {/* <Footer/> */}
               {/* </ContentContainer> */}
+              <WinNotification inputValue={inputValue} diceValue={diceValue} />
+              <DiceRoll onDiceValueChange={handleDiceValueChange}/>
+              <BetInput onInputValueChange={handleInputValueChange}/>
             </div>
           </ContextProvider>
+              
+              
         </>
     );
 };
-
+{/* <DiceRoll/> */}
 export default App;
