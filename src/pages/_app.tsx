b3f3a4273b366becf670571f9548951d6e38a3e1
Ignat <i3378047@gmail.com>
2023-09-08 18:46:19 +0200

    ignore useless imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,11 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { FC, useState } from 'react';
-// import { ContextProvider } from '../contexts/ContextProvider';
-// import { WalletProvider } from '@solana/wallet-adapter-react';
+import { ContextProvider } from '../contexts/ContextProvider';
 import { AppBar } from '../components/AppBar';
-import DiceRoll from '../components/DiceRoll'
-import BetInput from 'components/BetInput';
-import WinNotification from 'components/WinNotification'
-import Recent from 'components/Recent'
-import {Wallet} from 'Wallet'
-import { ContextProvider } from 'contexts/ContextProvider';
-// import { ContentContainer } from '../components/ContentContainer';
-// import { Footer } from '../components/Footer';
-// import Notifications from '../components/Notification'
+import WinNotification from 'components/WinNotification';
+import DiceRoll from '../components/DiceRoll';
+import BetInput from '../components/BetInput'
 require('@solana/wallet-adapter-react-ui/styles.css');
 require('../styles/globals.css');
 
@@ -29,11 +22,11 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
   }
   return (
     <>
-      <Head children={''}>
+      <Head>
         <title>Kaka i PIPI</title>
       </Head>
   
-      <ContextProvider children={''}>
+      <ContextProvider>
         <div className="flex flex-col h-screen">
           {/* <Notifications /> */}
           <AppBar />
@@ -44,7 +37,6 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
           <WinNotification inputValue={inputValue} diceValue={diceValue} />
           <DiceRoll onDiceValueChange={handleDiceValueChange} />
           <BetInput onInputValueChange={handleInputValueChange} />
-          <Wallet children={''}/>
         </div>
       </ContextProvider>
     </>
