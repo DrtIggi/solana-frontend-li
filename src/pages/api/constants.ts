d07e59cb4fa4c34e4f258c83319805ebf20eee32
Ignat <i3378047@gmail.com>
2023-09-10 05:39:29 +0200

    add vendor_provider in constants

--- src/pages/api/constants.ts
+++ src/pages/api/constants.ts
@@ -1,7 +1,7 @@
 import {idl} from "./dice_roll"
 import { Connection, PublicKey, clusterApiUrl, Keypair } from "@solana/web3.js";
 // import {Wallet}  from "@project-serum/anchor";
-
+import * as anchor from "@project-serum/anchor";
 export const commitmentLevel = "processed";
 export const endpoint = "https://api.devnet.solana.com";
 export const connection = new Connection(endpoint, commitmentLevel);
@@ -17,7 +17,13 @@ const t = require("@project-serum/anchor")
 import NodeWallet from '@project-serum/anchor/dist/cjs/nodewallet'
 const secret = Uint8Array.from(json_secret);
 const vendor = Keypair.fromSecretKey(secret);
-export let vendor_wallet = new NodeWallet(vendor)
+export const vendor_wallet = new NodeWallet(vendor)
+const preflightCommitment = "processed";
+const commitment = "processed";
+export const vendor_provider = new anchor.AnchorProvider(connection, vendor_wallet , {
+    preflightCommitment,
+    commitment,
+    });
 
 // const y = 'utils/stake.json'
 // export const stake = Uint8Array.from(JSON.parse(fs.readFileSync(y, 'utf8')));
